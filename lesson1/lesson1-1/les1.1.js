9b2b60412962e7ebb352b4b5961063c8e77efd9c
wkkrch <wkkrch@gmail.com>
2025-02-08 17:49:43 +0100

    сделала дз 1.1

---
+++ lesson1/lesson1-1/les1.1.js
@@ -0,0 +1,33 @@
+let a;
+a = 'hello';
+console.log(a);
+let b;
+b = 'owu';
+console.log(b);
+let c;
+c = 'com';
+console.log(c);
+let d;
+d = 'ua';
+console.log(d);
+
+let n1 = 1;
+console.log(n1);
+let n2 = 10;
+console.log(n2);
+let n3 = -999;
+console.log(n3);
+let n4 = 123;
+console.log(n4);
+let n5 = 3.14;
+console.log(n5);
+let n6 = 2.7;
+console.log(n6);
+let n7 = 16;
+console.log(n7);
+
+let b1 = true;
+console.log(b1);
+let b2 = false;
+console.log(b2);
+
