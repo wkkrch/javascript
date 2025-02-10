e05ba62ee8d7f838b22a5b12f8a37b06b385b293
wkkrch <wkkrch@gmail.com>
2025-02-10 03:45:38 +0100

    сделала дз 2.8

---
+++ lesson2/lesson2-8/les2.8.js
@@ -0,0 +1,12 @@
+let day = 19;
+if (day >= 1 && day <= 10) {
+    console.log('Перша декада мiсяця');
+}
+else if (day >= 11 && day <= 20) {
+    console.log('Друга декада мiсяця');
+}
+else if (day >= 21 && day <= 31) {
+    console.log('Третя декада мiсяця');
+} else {
+    console.log('Ви ввели невiрне число');
+}
