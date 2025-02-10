283a2142f18ed27d1c022d628885aab1dc68db7a
wkkrch <wkkrch@gmail.com>
2025-02-10 21:45:20 +0100

    сделала дз 2.12

---
+++ lesson2/lesson2-12/les2.12.js
@@ -0,0 +1,13 @@
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+coursesAndDurationArray.forEach(course => {
+    if (course.monthDuration > 5) {
+        console.log(course.title + " - Супер");
+    }
+});
