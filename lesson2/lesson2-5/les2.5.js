2f782a3f00474e252453f8edfe73a59ebf851fe2
wkkrch <wkkrch@gmail.com>
2025-02-10 02:58:48 +0100

    сделала дз 2.5

---
+++ lesson2/lesson2-5/les2.5.js
@@ -0,0 +1,9 @@
+let temperatures = [
+    { day: "Monday", morning: 10, afternoon: 15, evening: 12 },
+    { day: "Tuesday", morning: 9, afternoon: 14, evening: 11 },
+    { day: "Wednesday", morning: 8, afternoon: 13, evening: 10 },
+    { day: "Thursday", morning: 11, afternoon: 16, evening: 13 },
+    { day: "Friday", morning: 12, afternoon: 17, evening: 14 },
+    { day: "Saturday", morning: 10, afternoon: 15, evening: 12 },
+    { day: "Sunday", morning: 9, afternoon: 14, evening: 11 }
+];
