d13e6963c923fe07064ab74602e932300bc80471
wkkrch <wkkrch@gmail.com>
2025-02-10 02:46:14 +0100

    сделала дз 2.4

---
+++ lesson2/lesson2-4/les2.4.js
@@ -0,0 +1,22 @@
+let users = [
+    {name: 'Anna', username: 'cat123', password: 'annacat62738'},
+    {name: 'Kate', username: 'katyalala', password: 'gdhsodpdks1029'},
+    {name: 'William', username: 'voin62', password: 'asdfqwer0147'},
+    {name: 'Kleo', username: 'kleopatra', password: 'hdjfksla111'},
+    {name: 'Gorge', username: 'valinski', password: 'go01va01linski'},
+    {name: 'Kristi', username: 'poni12', password: 'afHsjalJ251'},
+    {name: 'Rick', username: 'faraon3', password: 'jaiskJjaksk'},
+    {name: 'Marta', username: 'martalove', password: 'ahsiOpsksha'},
+    {name: 'Joshua', username: 'bond45', password: 'jaksksjJkaksj'},
+    {name: 'Roberto', username: 'robwvwvw', password: 'Kajdduhs12'}
+];
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
